ba7da1a20e77490afef3d0f0e052b08dfde502c6
Abner <87242234+Krollynk@users.noreply.github.com>
2025-11-18 23:11:12 -0600

    view paises

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/config/data_source.php';
-require_once __DIR__ . '/core/ModelMapper.php';
+require_once __DIR__ . '/models/ModelMapper.php';
 require_once __DIR__ . '/core/DatabaseExecutor.php';
 require_once __DIR__ . '/core/QueryBuilder.php';
 require_once __DIR__ . '/core/Router.php';
