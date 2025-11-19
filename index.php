240819a81ce82268fa267a5c7b12f45e7c156c56
Abner <87242234+Krollynk@users.noreply.github.com>
2025-11-18 20:29:47 -0600

    router request and response

--- index.php
+++ index.php
@@ -3,3 +3,6 @@ require_once __DIR__ . '/config/data_source.php';
 require_once __DIR__ . '/core/ModelMapper.php';
 require_once __DIR__ . '/core/DatabaseExecutor.php';
 require_once __DIR__ . '/core/QueryBuilder.php';
+require_once __DIR__ . '/core/Router.php';
+require_once __DIR__ . '/core/Request.php';
+require_once __DIR__ . '/core/Response.php';
